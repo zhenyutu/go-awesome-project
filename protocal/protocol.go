88b32d0e5d8b3c220066e64da7a84f67bae20d36
zhenyutu <1572629866@qq.com>
2025-02-10 16:16:07 +0800

    add protocol test

---
+++ protocal/protocol.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"awesomeProject/protocal/student"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+)
+
+func main() {
+	test := student.Student{
+		Name:   "hello",
+		Male:   true,
+		Scores: []int32{98, 85, 88},
+	}
+
+	data, err := proto.Marshal(&test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	fmt.Println("marshal result: ", string(data))
+
+	newTest := &student.Student{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	fmt.Println("unmarshal result: ", newTest)
+
+	if test.Male != newTest.Male {
+		log.Fatal("test male not equal")
+	}
+}
