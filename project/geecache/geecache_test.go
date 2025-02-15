c16e5c369c79150b60da04fabc7574f65df2ca1c
zhenyutu <1572629866@qq.com>
2025-02-15 17:17:48 +0800

    add  http server cache test

--- project/geecache/geecache_test.go
+++ project/geecache/geecache_test.go
@@ -2,6 +2,7 @@ package geecache
 
 import (
 	"log"
+	"net/http"
 	"testing"
 )
 
@@ -17,4 +18,15 @@ func TestGroupCache(test *testing.T) {
 	test.Log(group.Get("key1"))
 	test.Log(group.Get("key4"))
 	test.Log(group.Get("key4"))
+}
+
+func TestGroupServer(test *testing.T) {
+	group := NewGroup("test", 1024, GetterFunc(func(key string) ([]byte, error) {
+		log.Println("load db")
+		return []byte("load by db"), nil
+	}))
+
+	addr := "localhost:9999"
+	dispatcher := group.newHttpServer(addr)
+	log.Fatal(http.ListenAndServe(addr, dispatcher))
 }
