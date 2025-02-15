f25d348cbedbdd74bbe5704e4c63cadd386320b4
zhenyutu <1572629866@qq.com>
2025-02-15 16:41:54 +0800

    add  group cache test

--- project/geecache/lru/lru_test.go
+++ project/geecache/lru/lru_test.go
@@ -10,9 +10,9 @@ func (c String) Len() int64 { return int64(len(string(c))) }
 
 func TestCache(test *testing.T) {
 	cache := New(20)
-	cache.put("key1", String("value1"))
+	cache.Put("key1", String("value1"))
 
-	if v, ok := cache.get("key1"); !ok || v.(String) != "value1" {
+	if v, ok := cache.Get("key1"); !ok || v.(String) != "value1" {
 		test.Fatal("cache put get check fail")
 	}
 
@@ -20,13 +20,13 @@ func TestCache(test *testing.T) {
 
 func TestLru(t *testing.T) {
 	cache := New(20)
-	cache.put("key1", String("value1"))
-	cache.put("key2", String("value2"))
-	cache.put("key3", String("value3"))
-	cache.put("key4", String("value4"))
+	cache.Put("key1", String("value1"))
+	cache.Put("key2", String("value2"))
+	cache.Put("key3", String("value3"))
+	cache.Put("key4", String("value4"))
 
-	t.Log(cache.get("key1"))
-	t.Log(cache.get("key2"))
-	t.Log(cache.get("key3"))
-	t.Log(cache.get("key4"))
+	t.Log(cache.Get("key1"))
+	t.Log(cache.Get("key2"))
+	t.Log(cache.Get("key3"))
+	t.Log(cache.Get("key4"))
 }
